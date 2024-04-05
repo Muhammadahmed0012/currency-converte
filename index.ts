1eaee434ade51ee80cae4ea94842eb2160427e9f
Muhammad Ahmed <ahmedarain0012@gmail.com>
2024-04-06 02:19:39 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+const currency: any = {
+  USD: 1,
+  EUR: 0.91,
+  GBP: 0.76,
+  INR: 74.57,
+  PKR: 280,
+};
+let user_answer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter from currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "to",
+    message: "Enter to currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "amount",
+    message: "Enter from currency",
+    type: "number",
+  },
+]);
+
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+let convertAmount = baseAmount * toAmount;
+console.log(convertAmount.toFixed(2));
